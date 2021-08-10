a91237ea6f8fb0987d90988ca45a3bd759caa8fe
ongaram <ohjiho0106@gmail.com>
2021-08-10 22:25:44 +0900

    first commit

--- main.js
+++ main.js
@@ -1,46 +1,15 @@
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
-
-var colorChange = document.querySelector('.color');
-var content = document.querySelectorAll('.content');
-var number = document.querySelectorAll('.number');
-var title = document.querySelectorAll('.title');
-var sidebarBtn = document.querySelector('.sidebarBtn');
-var sidebarBtnImage = document.querySelector('.sidebarBtnImage');
-var sidebar = document.querySelector('.sidebar');
-var dayNightImage = document.querySelectorAll('.dayNightImage');
-var items = document.querySelectorAll('.question');
-
-function openCloseAnswer() {
-    const answerId = this.id.replace('que', 'ans');
-
-    if(document.getElementById(answerId).style.display === 'block') {
-        document.getElementById(answerId).style.display = 'none';
-        document.getElementById(this.id + '-toggle').textContent = '+';
-    } else {
-        document.getElementById(answerId).style.display = 'block';
-        document.getElementById(this.id + '-toggle').textContent = '-';
-    }
-}
-
-  items.forEach(item => item.addEventListener('click', openCloseAnswer));
+const colorChange = document.querySelector('.color');
+const content = document.querySelectorAll('.content');
+const number = document.querySelectorAll('.number');
+const title = document.querySelectorAll('.title');
+const red = document.querySelectorAll('.red');
+const body = document.querySelector('body');
 
 function nightDayHandler(self) {
 
-    var body = document.querySelector('body');
-
     if (self.value === 'night') {
         body.style.backgroundImage = 'url("img_night.jpg")';
-        body.style.backgroundSize = 'cover';
-        colorChange.innerText = ('🤞ㅤㅤ야간모드ㅤㅤ🤞');
+        body.style.backgroundSize = 'initial';
         self.value = 'day';
         setColor1('white');
         setColor2('white');
@@ -48,10 +17,11 @@ function nightDayHandler(self) {
         setColor4('rgba(0, 0, 0, 0)');
         setColor5('rgba(0, 0, 0, 0)');
         setColor6('white');
-    } else {
+        setColor7('rgba(0, 0, 0, 0)');
+        setColor8('white');
+    } else if (self.value === 'day') {
         body.style.backgroundImage = 'url("img_day.jpg")';
-        body.style.backgroundSize = 'cover';
-        colorChange.innerText = ('🤞ㅤㅤ주간모드ㅤㅤ🤞');
+        body.style.backgroundSize = 'initial';
         self.value = 'night';
         setColor1('#2DB400');
         setColor2('rgb(85, 50, 0)');
@@ -59,60 +29,63 @@ function nightDayHandler(self) {
         setColor4('white');
         setColor5('white');
         setColor6('rgb(248, 166, 72)');
+        setColor7('white');
+        setColor8('#CD2E57');
     }
 }
 
 function setColor1(color1) {
 
-    for (var c1 = 0; c1 < content.length; c1++) {
+    for (let c1 = 0; c1 < content.length; c1++) {
         content[c1].style.color = color1;
     }
 }
 
 function setColor2(color2) {
     
-    for (var c2 = 0; c2 < number.length; c2++) {
+    for (let c2 = 0; c2 < number.length; c2++) {
         number[c2].style.color = color2;
     } 
 }
 
 function setColor3(color3) {
     
-    for (var c3 = 0; c3 < content.length; c3++) {
+    for (let c3 = 0; c3 < content.length; c3++) {
         content[c3].style.backgroundColor = color3;
     }
 }
 
 function setColor4(color4) {
     
-    for (var c4 = 0; c4 < number.length; c4++) {
+    for (let c4 = 0; c4 < number.length; c4++) {
         number[c4].style.backgroundColor = color4;
     } 
 }
 
 function setColor5(color5) {
     
-    for (var c5 = 0; c5 < title.length; c5++) {
+    for (let c5 = 0; c5 < title.length; c5++) {
         title[c5].style.backgroundColor = color5;
     }
 }
 
 function setColor6(color6) {
     
-    for (var c6 = 0; c6 < title.length; c6++) {
+    for (let c6 = 0; c6 < title.length; c6++) {
         title[c6].style.color = color6;
     }
 }
 
+function setColor7(color7) {
+    
+    for (let c7 = 0; c7 < red.length; c7++) {
+        red[c7].style.backgroundColor = color7;
+    }
+}
 
-sidebarBtn.addEventListener('click', () => {
+function setColor8(color8) {
 
-    sidebar.classList.toggle('active');
-    sidebarBtn.classList.toggle('toggle');
-
-    if(sidebar.classList.length == 2) {
-        sidebarBtnImage.src = 'left.svg';
-    } else {
-        sidebarBtnImage.src = 'right.svg';
-    };
-});
+    for (let c8 = 0; c8 < red.length; c8++) {
+        red[c8].style.color = color8;
+    }
+}
